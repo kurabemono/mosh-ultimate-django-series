2c3b19998d52458bab22a3e63c46e5bdda4bd5a6
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-13 10:14:42 +0900

    Ch5-4 Managers and Querysets

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from store.models import Product
 
 
 def say_hello(request):
-    x = calculate()
+    query_set = Product.objects.all()
+
+    for product in query_set:
+        print(product)
+
     return render(request, 'hello.html', {'name': 'Mosh'})
