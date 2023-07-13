0a202d711cb953a4fa5e69a73431337700efef71
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-13 12:58:20 +0900

    Ch5-14 Aggregating objects

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.db.models import Q, F
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, Order, OrderItem
 
 
 def say_hello(request):
-    # select_related(1)
-    # prefetch_related(n)
-    query_set = Order.objects.select_related(
-        'customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    result = Product.objects.filter(collection__id=1).aggregate(
+        count=Count('id'), min_price=Min('unit_price'))
 
-    return render(request, 'hello.html', {'name': 'Mosh', 'orders': list(query_set)})
+    return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
