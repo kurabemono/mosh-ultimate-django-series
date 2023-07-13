a20dcaaeb01bcd1273b9bc6a50fbbd7a5ac7ba57
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-13 10:43:36 +0900

    Ch5-7 Lookups using Q objects

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
 
 def say_hello(request):
-    product = Product.objects.filter(pk=0).first()
+    # Products: inventory < 10 and price < 20
+    query_set = Product.objects.filter(
+        Q(inventory__lt=10) | ~Q(unit_price__lt=20))
 
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
