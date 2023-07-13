b6679af1dc147479599e44ce4aa35f8d846c8843
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-13 11:01:00 +0900

    Ch5-10 Limiting results

--- playground/views.py
+++ playground/views.py
@@ -5,6 +5,6 @@ from store.models import Product
 
 def say_hello(request):
     # Products: inventory < 10 and price < 20
-    query_set = Product.objects.filter(inventory=F('collection__id'))
+    query_set = Product.objects.all()[:5]
 
     return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
