632e6388e20e0ce7aecbdfd8171eea3a75767e76
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-13 12:34:51 +0900

    Ch5-11 Selecting fields to query

--- playground/views.py
+++ playground/views.py
@@ -5,6 +5,7 @@ from store.models import Product
 
 def say_hello(request):
     # Products: inventory < 10 and price < 20
-    query_set = Product.objects.all()[:5]
+    query_set = Product.objects.values('id', 'title', 'orderitem__product__id').filter(
+        orderitem__product__id__isnull=False).distinct().order_by('title')
 
     return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
