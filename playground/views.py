591c859eb04dc422b4a637afd661c0a2555f12f2
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-14 15:11:34 +0900

    Ch5-16 Calling database functions

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from django.db.models import Value, F
+from django.db.models import Value, F, Func
+from django.db.models.functions import Concat
 from store.models import Product, Customer
 
 
 def say_hello(request):
-    query_set = Customer.objects.annotate(new_id=F('id')+1)
+    # query_set = Customer.objects.annotate(
+    #     # CONCAT, not supported in SQLite
+    #     full_name=(Func(F('first_name'), Value(' '),
+    #                F('last_name'), function='CONCAT'))
+    # )
+
+    query_set = Customer.objects.annotate(
+        # supported by SQLite through || operator transparently
+        full_name=Concat('first_name', Value(' '), 'last_name')
+    )
 
     return render(request, 'hello.html', {'name': 'Mosh', 'result': list(query_set)})
