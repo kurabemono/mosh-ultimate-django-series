fdd758432774e483aa103d5f2f02b1fbb375b3f0
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-14 21:32:39 +0900

    Ch6-9 Adding search

--- store/admin.py
+++ store/admin.py
@@ -29,6 +29,7 @@ class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
     def orders_count(self, customer: models.Customer):
