6dc7c36b668d55bb4e38b9164fed10b1065a7d27
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-14 15:27:02 +0900

    Ch5-19 Querying generic relationships

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.db.models import Value, F, ExpressionWrapper, DecimalField
-from django.db.models.functions import Concat
-from store.models import Product, Customer
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(
-        F('unit_price') * 0.8, output_field=DecimalField())
-    query_set = Product.objects.annotate(
-        discounted_price=discounted_price
-    )
+    content_type = ContentType.objects.get_for_model(Product)
+
+    query_set = TaggedItem.objects.select_related('tag').filter(
+        content_type=content_type, object_id=1)
 
     return render(request, 'hello.html', {'name': 'Mosh', 'result': list(query_set)})
