bb030bf864cf60a10b95e9080382e7907d698395
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-14 15:14:58 +0900

    Ch5-17 Grouping data

--- playground/views.py
+++ playground/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func
+from django.db.models import Value, F, Func, Count
 from django.db.models.functions import Concat
 from store.models import Product, Customer
 
 
 def say_hello(request):
-    # query_set = Customer.objects.annotate(
-    #     # CONCAT, not supported in SQLite
-    #     full_name=(Func(F('first_name'), Value(' '),
-    #                F('last_name'), function='CONCAT'))
-    # )
-
     query_set = Customer.objects.annotate(
-        # supported by SQLite through || operator transparently
-        full_name=Concat('first_name', Value(' '), 'last_name')
+        orders_count=Count('order')
     )
 
     return render(request, 'hello.html', {'name': 'Mosh', 'result': list(query_set)})
