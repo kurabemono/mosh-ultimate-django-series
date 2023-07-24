539e0dfb94f7e48979c1ee1812d7d8543aff0b28
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-24 15:52:53 +0900

    Ch3-9 Nested routers

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 from . import views
+from pprint import pprint
 
-router = SimpleRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
-urlpatterns = router.urls
+products_router = routers.NestedDefaultRouter(
+    router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet,
+                         basename='product-reviews')
+
+urlpatterns = router.urls + products_router.urls
+pprint(urlpatterns)
