011e6ae23c632d3e4f61de71bd74a3578f5015a4
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-25 13:45:19 +0900

    Ch5-5 Creating user profiles

--- store/admin.py
+++ store/admin.py
@@ -59,7 +59,8 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
