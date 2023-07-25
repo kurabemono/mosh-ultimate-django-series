ee71ee57d5837e45e176b3d55174de50cc5056c7
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-25 13:35:45 +0900

    Ch5-4 Extending the user model

--- storefront/settings.py
+++ storefront/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'playground',
     'store',
-    'store_custom',
     'tags',
-    'likes'
+    'likes',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -140,3 +140,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 }
+
+AUTH_USER_MODEL = 'core.User'
