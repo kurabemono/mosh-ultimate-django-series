61bdf4f6890cf1929224aa8671f61a638ba6f7e8
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-26 13:10:59 +0900

    Ch7-12 Creating custom signals

--- store/serializers.py
+++ store/serializers.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.db import transaction
 from rest_framework import serializers
 from .models import Cart, CartItem, Collection, Customer, Order, OrderItem, Product, Review
+from .signals import order_created
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -160,4 +161,5 @@ class CreateOrderSerializer(serializers.Serializer):
             ]
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=cart_id).delete()
+            order_created.send_robust(self.__class__, order=order)
             return order
