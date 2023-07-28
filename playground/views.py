1b170fc7c86b50d41f10e9c3f2d6c0bbaa6d572a
Dion Melosantos <6504404+kurabemono@users.noreply.github.com>
2023-07-28 13:03:40 +0900

    Ch3 Sending templated emails

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.core.mail import EmailMessage, BadHeaderError
+from templated_mail.mail import BaseEmailMessage
 
 
 # @transaction.atomic()
 def say_hello(request):
     try:
-        message = EmailMessage('subject', 'message',
-                               'from@moshbuy.com', ['john@moshbuy.com'])
-        message.attach_file('playground/static/images/munchkin.jpg')
-        message.send()
+        # message = EmailMessage('subject', 'message',
+        #                        'from@moshbuy.com', ['john@moshbuy.com'])
+        # message.attach_file('playground/static/images/munchkin.jpg')
+        # message.send()
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={
+                'name': 'Mosh'
+            }
+        )
+        message.send(['john@moshbuy.com'])
     except BadHeaderError:
         pass
 
