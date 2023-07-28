fd6d0ea4b56dc2d911dabcc5cb0569de809c088f
kurabemono <6504404+kurabemono@users.noreply.github.com>
2023-07-28 15:31:56 +0900

    Ch4 Creating and executing tasks

--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-from django.core.mail import EmailMessage, BadHeaderError
-from templated_mail.mail import BaseEmailMessage
+from .tasks import notify_customers
 
 
 # @transaction.atomic()
 def say_hello(request):
-    try:
-        # message = EmailMessage('subject', 'message',
-        #                        'from@moshbuy.com', ['john@moshbuy.com'])
-        # message.attach_file('playground/static/images/munchkin.jpg')
-        # message.send()
-        message = BaseEmailMessage(
-            template_name='emails/hello.html',
-            context={
-                'name': 'Mosh'
-            }
-        )
-        message.send(['john@moshbuy.com'])
-    except BadHeaderError:
-        pass
+    notify_customers.delay('Hello')
 
     return render(request, 'hello.html', {'name': 'Mosh'})
