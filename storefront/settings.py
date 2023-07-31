81d0abf1a83816d8db750776389a6e1c47a44b6e
kurabemono <6504404+kurabemono@users.noreply.github.com>
2023-07-31 11:27:41 +0900

    Ch8 Install whitenoise

--- storefront/settings.py
+++ storefront/settings.py
@@ -54,8 +54,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
